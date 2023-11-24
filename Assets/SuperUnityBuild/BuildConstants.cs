0ec9487f37cdeaa7a2a48e8dcdc67574106f975a
Stephen Phillips <kingcoyote85@gmail.com>
2023-11-24 06:52:19 -0800

    Misc updates, as well as updating Unity to the newest 2022 LTS.

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,47 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+namespace SuperUnityBuild.Generated
+{
+    public enum ReleaseType
+    {
+        None,
+        Standard,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+    }
+
+    public enum ScriptingBackend
+    {
+        None,
+        Mono,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x86,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static class BuildConstants
+    {
+        public static readonly DateTime buildDate = new DateTime(638324573272207037);
+        public const string version = "0.1.1";
+        public const ReleaseType releaseType = ReleaseType.Standard;
+        public const Platform platform = Platform.PC;
+        public const ScriptingBackend scriptingBackend = ScriptingBackend.Mono;
+        public const Architecture architecture = Architecture.Windows_x86;
+        public const Distribution distribution = Distribution.None;
+    }
+}
+
