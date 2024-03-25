98dbb69fab8dfe0af2673ac209c2345b96af8816
Stephen Phillips <kingcoyote85@gmail.com>
2024-03-25 13:00:06 -0700

    Adding version to the top menu.

--- Assets/_Potion Blues/Shop/UI/TopMenuPanelScript.cs
+++ Assets/_Potion Blues/Shop/UI/TopMenuPanelScript.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TopMenuPanelScript : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _version;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _version.text = $"Version {Application.version}";
     }
 
     // Update is called once per frame
