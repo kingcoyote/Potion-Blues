5f9055117f5b6165034b7a9f0f08727f8c890fc6
Stephen Phillips <kingcoyote85@gmail.com>
2024-04-12 05:56:00 -0700

    Creating an operate script that you click andd hold to do brewing, cleaning, ingredient swapping, etc.

---
+++ Assets/_Potion Blues/Shop/Player/OperateScript.cs
@@ -0,0 +1,100 @@
+using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using VectorSwizzling;
+
+namespace PotionBlues.Shop
+{
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class OperateScript : MonoBehaviour
+    {
+        public bool IsComplete => _operatingTime >= _requiredOperatingTime;
+        public Action OnComplete = () => { };
+
+        private float _requiredOperatingTime;
+        private float _fillSpeed = 1;
+        private float _decaySpeed = 1;
+
+        private PlayerInput _input;
+        private bool _isOperating;
+        private float _operatingTime;
+
+        [SerializeField] private CircleCollider2D _box;
+        [SerializeField] private Image _progressBar;
+        [SerializeField] private Image _icon;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _input = GameObject.Find("Player").GetComponent<PlayerInput>();
+            _input.actions["Select"].performed += OnSelect;
+            _input.actions["Select"].canceled += OnCancel;
+
+            _box = GetComponent<CircleCollider2D>();
+        }
+
+        private void OnDestroy()
+        {
+            if (_input != null)
+            {
+                _input.actions["Select"].performed -= OnSelect;
+                _input.actions["Select"].canceled -= OnCancel;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (IsComplete)
+            {
+                return;
+            }
+
+            if (_isOperating)
+            {
+                _operatingTime += Time.deltaTime * _fillSpeed;
+            } else
+            {
+                _operatingTime -= Time.deltaTime * _decaySpeed;
+            }
+
+            _operatingTime = Mathf.Clamp(_operatingTime, 0, _requiredOperatingTime);
+            _progressBar.fillAmount = _operatingTime / _requiredOperatingTime;
+
+            if (_operatingTime >= _requiredOperatingTime)
+            {
+                OnComplete.Invoke();
+            }
+        }
+
+        private void OnSelect(InputAction.CallbackContext context)
+        {
+            var cursor = Camera.main.ScreenToWorldPoint(_input.actions["Cursor"].ReadValue<Vector2>()).xy();
+            if (_box.bounds.Contains(cursor) == false) return;
+
+            _isOperating = true;
+        }
+
+        private void OnCancel(InputAction.CallbackContext context)
+        {
+            _isOperating = false;
+        }
+
+        public void Set(Sprite icon, float operatingTime)
+        {
+            _icon.sprite = icon;
+            _requiredOperatingTime = operatingTime;
+            _operatingTime = 0;
+        }
+
+        [Button]
+        public void Set(Sprite icon, float operatingTime, float fillSpeed, float decaySpeed)
+        {
+            Set(icon, operatingTime);
+            _fillSpeed = fillSpeed;
+            _decaySpeed = decaySpeed;
+        }
+    }
+}
